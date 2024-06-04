5f221c180f789d56a4a1cfac47fbb575a5b45c1e
Naveen <naveen.ndr97@gmail.com>
2024-06-04 07:48:25 +0530

    lazy loading and Suspense

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { createBrowserRouter } from "react-router-dom";
-import Counter from "./component/counter";
+// import Counter from "./component/counter";
 import Post from "./component/post";
 import Contact from "./component/contact";
 import UsersList from "./component/usersList";
@@ -9,13 +10,15 @@ import ErrorPage from "./component/errorPage";
 import PrivateRoute from "./component/privateRoute";
 import Login from "./component/login";
 
+const Counter = React.lazy(() => import("./component/counter"))
+
 const routes = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
         errorElement: <ErrorPage />,
         children: [
-            { index: true, element: <Counter /> }, //index true says that when route is at '/' Counter component has to render
+            { index: true, element: <React.Suspense fallback={<>...</>}><Counter /> </React.Suspense> }, //index true says that when route is at '/' Counter component has to render
             { path: '/login', element: <Login /> },
             { path: '/post', element: <Post /> },
             { path: '/contact', element: <Contact /> },
